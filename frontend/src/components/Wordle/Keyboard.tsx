3554e46d201b4e26ddd3feac017bb85cd1b880b3
Rageven <salif.magassa18@laposte.net>
2026-05-02 21:19:36 +0200

    feat: implement keyboard with images for Enter and Backspace, update to-do list, for images import new file custom.d.ts (TEST IF USEFULL), and implementation of a test in the rules of webpack.cjs

--- frontend/src/components/Wordle/Keyboard.tsx
+++ frontend/src/components/Wordle/Keyboard.tsx
@@ -1,23 +1,48 @@
 import type  { PuzzleStoreType } from "./PuzzleStore";
 import { observer, useLocalObservable } from "mobx-react-lite";
+import enterImg from './img/symbole-de-la-fleche-gauche-dans-un-cercle.png';
+import backspaceImg from './img/retour-arriere.png';
 
 
 export default function Keyboard({ store }: { store: PuzzleStoreType }){	
 
-	// const qwerty = ['qwertyuiop', 'asdfghjkl', '', 'zxcvbnm', '']
-	const qwerty = ['qwertyuiop', 'asdfghjkl', 'zxcvbnm']
-	return (
-  	<div> 
-		{qwerty.map((row) => (
+	const qwerty = [
+	'qwertyuiop',
+	'asdfghjkl',
+	['Enter', ...'zxcvbnm'.split(''), 'Backspace'],
+	];
+	const wideKeys = new Set(["Backspace", "Enter"]);
+	let isWide : boolean
 
-  				<div className="flex justify-center"> 
-				{row.split('').map((key) => (
-					<div className="m-px flex h-12 w-9.5 rounded-md bg-gray-400  font-bold text-2xl items-center justify-center">
-						<button className="uppercase" onClick={() => (store.handleKeyboard(key))}> {key} </button>
-					</div>
-				))}
-				</div>
+	//pour les map ajt potenciellemnt les index (apart pr les couleurs inutile)
+	return (
+	<div>
+		{qwerty.map((row) => (
+		<div className="flex justify-center">
+			{(Array.isArray(row) ? row : row.split('')).map((key) => (
+			isWide = wideKeys.has(key),
+
+			<div
+			className={[
+				"m-px flex h-12 rounded-md bg-gray-400 font-bold text-2xl items-center justify-center",
+				isWide ? "w-15" : "w-9.5",
+			].join(" ")}
+			>
+
+			<button className="uppercase" onClick={() => store.handleKeyboard(key)} >
+			{key === 'Enter' ? (
+				<img src={enterImg} alt="enter" className="h-7 w-7" />
+			) : key === 'Backspace' ? (
+				<img src={backspaceImg} alt="return" className="h-7 w-7" />
+			) : (
+				key
+			)}
+			</button>
+			
+			</div>
 			))}
+		</div>
+		))}
 	</div>
-	)
+	);
 }
